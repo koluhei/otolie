0b1678e3e0ef658c6e96e051f46e23a0dac89add
koluhei <qpal79@gmail.com>
2021-07-30 14:20:14 +0900

    ユーザー編集機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :image])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile, :image])
   end
-
-  def sign_out_user
-    sign_out_and_redirect(current_user) if creator_signed_in?
-  end
-  def sign_out_creator
-    sign_out_and_redirect(current_creator) if user_signed_in?
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root to: "songs#index"
   resources :songs
   resources :creators, only: [:show]
+  resources :users, only: [:show]
 end
